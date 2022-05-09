5849f1a4bea450dc59bc909dbc7f26eeec4e3f48
brijesh-potnuru <97963480+brijesh-potnuru@users.noreply.github.com>
2022-05-09 22:28:32 +0530

    Add files via upload

---
+++ Checkin-MicroService/src/main/java/caseStudy/checkinMicroService/CheckinMicroServiceApplication.java
@@ -0,0 +1,24 @@
+package caseStudy.checkinMicroService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableSwagger2
+public class CheckinMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckinMicroServiceApplication.class, args);
+	}
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+	    return builder.build();
+	}
+}
